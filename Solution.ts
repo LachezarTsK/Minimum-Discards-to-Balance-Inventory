24e4365d722b3bc8b6279bf5f614950f8d72ace4
Lachezar Kutsarov <lkutsarov@gmail.com>
2026-05-06 14:43:15 +0200

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,35 @@
+
+function minArrivalsToDiscard(itemsArrival: number[], windowSize: number, maxNumberSameItemsInWindow: number): number {
+    const REMOVED_ITEM = -1;
+    const maxItemValue = Math.max(...itemsArrival);
+    let numberOfDiscards = 0;
+    const frequencyItems: number[] = new Array(maxItemValue + 1).fill(0);
+
+    for (let i = 0; i < windowSize; ++i) {
+        ++frequencyItems[itemsArrival[i]];
+        if (frequencyItems[itemsArrival[i]] > maxNumberSameItemsInWindow) {
+            ++numberOfDiscards;
+            --frequencyItems[itemsArrival[i]];
+            itemsArrival[i] = REMOVED_ITEM;
+        }
+    }
+
+    let back = 0;
+    let front = windowSize;
+
+    while (front < itemsArrival.length) {
+        if (itemsArrival[back] !== REMOVED_ITEM) {
+            --frequencyItems[itemsArrival[back]];
+        }
+        ++frequencyItems[itemsArrival[front]];
+
+        if (frequencyItems[itemsArrival[front]] > maxNumberSameItemsInWindow) {
+            ++numberOfDiscards;
+            --frequencyItems[itemsArrival[front]];
+            itemsArrival[front] = REMOVED_ITEM;
+        }
+        ++back;
+        ++front;
+    }
+    return numberOfDiscards;
+};
